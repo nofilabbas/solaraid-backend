e3c330eeb543366907295afbb0c87e268571423c
nofil abbad <nofilabbas.006@gmail.com>
2025-05-15 23:18:35 +0500

    Add STATIC ROOT and ALLOWED HOSTS to settings.py

--- backend_api/settings.py
+++ backend_api/settings.py
@@ -21,6 +21,8 @@ load_dotenv()  # load variables from .env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ALLOWED_HOSTS = ['*']  # for now, allow all, we'll fix later
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
